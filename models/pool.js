3813b8aa24c2ef688d0c042467c52c908e68c253
jcorriveau23 <60741764+jcorriveau23@users.noreply.github.com>
2021-02-05 21:57:31 -0500

    added pool creation schema

---
+++ models/pool.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const poolSchema = new Schema({
+  name: {type: String},
+  owner: {type: String},
+  number_poolers: {type: Number},
+  number_forward: {type: Number},
+  number_defenders: {type: Number},
+  number_goalies: {type: Number},
+
+  forward_pts_goals: {type: Number},
+  forward_pts_assists: {type: Number},
+  forward_pts_hattricks: {type: Number},
+  defender_pts_goals: {type: Number},
+  defender_pts_assits: {type: Number},
+  defender_pts_hattricks: {type: Number},
+  goalies_pts_wins: {type: Number},
+  goalies_pts_shutouts: {type: Number},
+  goalies_pts_goals: {type: Number},
+  goalies_pts_assists: {type: Number},
+
+  participants: [String],
+
+  context: {
+      participants: [{
+          user: {type: String},
+          choice_forward: [{name: {type: String}, api_call: {type: String}}],
+          choice_defender: [{name: {type: String}, api_call: {type: String}}],
+          choice_goalies: [{name: {type: String}, api_call: {type: String}}]
+      }]
+  }
+
+}, {timestamps: true})
+
+
+
+const User = mongoose.model('Pool', poolContextSchema)
+module.exports = User
+
