113688a14938e04773d09ada6b47f13ce9cb8bd2
jcorriveau23 <60741764+jcorriveau23@users.noreply.github.com>
2023-01-14 14:38:58 -0500

    Home page description nhl credit

--- client/src/pages/home_page.jsx
+++ client/src/pages/home_page.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // component
 import DayLeaders from '../components/home_page/dailyLeaders';
@@ -7,13 +8,18 @@ import SummaryLeaders from '../components/leagueLeaders_page/summaryLeaders';
 export default function HomePage({ formatDate, todayFormatDate, gameStatus, injury }) {
   return (
     <div className="cont">
-      <h1>Welcome to hockeypool.live.</h1>
-      <h3>It is now live on Kovan Network to connect your wallet and bet some Eth on hockey games.</h3>
-      <h3>
-        You can also watch live nhl game content, watch your favorite players stats, and create some classic or dynastie
-        type pool with your friends. Each months players are allowed to update their roster with their reservists and
-        trade are allowed throught the year.
-      </h3>
+      <div className="information-box">
+        <h1>Welcome to hockeypool.live</h1>
+        <p>
+          Where you can watch live nhl game content, watch your favorite players stats! All the players/teams/games
+          information is coming from the nhl api. Nothing is being monetize with this web site.
+        </p>
+        <h1>We manage pool for you</h1>
+        <p>The draft, trades and roster modifications processes are automated. So you have almost nothing to manage.</p>
+        <Link to="/my-pools" style={{ textDecoration: 'none', color: '#000099' }}>
+          <h2>Create Pool...</h2>
+        </Link>
+      </div>
       <SummaryLeaders injury={injury} statsType="points" type="skater" playerType="allSkaters" season="20222023" />
       <SummaryLeaders injury={injury} statsType="wins" type="goalie" playerType="GOnly" season="20222023" />
       <div>
