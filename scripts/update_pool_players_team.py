5d6a1fbd3bea321ec9b460b57ac4b98f8abc8c4f
jcorriveau23 <60741764+jcorriveau23@users.noreply.github.com>
2023-09-23 15:29:35 -0400

    fix script updating team players

--- scripts/update_pool_players_team.py
+++ scripts/update_pool_players_team.py
@@ -22,34 +22,32 @@ def update_pool_players_team():
     players_id_to_current_team_dict = {}  # This dict will store the player current team to avoid requesting it multiple time
 
     for pool in db.pools.find():
-        if pool["context"] is None or pool["context"]["pooler_roster"] is None:
+        if pool["context"] is None or pool["context"]["players"] is None:
             continue
 
-        for participant in pool["context"]["pooler_roster"]:
-            # print(participant)
-            for players_type_key in ["chosen_forwards", "chosen_defenders", "chosen_goalies", "chosen_reservists"]:
-                for i, player_id in enumerate(pool["context"]["pooler_roster"][participant][players_type_key]):
-                    # print(player)
-                    if players_id_to_current_team_dict.get(player_id) is None:
-                        player_url = 'https://statsapi.web.nhl.com/api/v1/people/' + str(player_id)
+        for player_id in pool["context"]["players"]:
+            # print(player)
+            if players_id_to_current_team_dict.get(player_id) is None:
+                player_url = f'https://statsapi.web.nhl.com/api/v1/people/{player_id}'
 
-                        response = requests.request('GET', player_url)
-                        player_info_json = json.loads(response.text)
+                response = requests.request('GET', player_url)
+                player_info_json = json.loads(response.text)
 
-                        if "currentTeam" not in player_info_json["people"][0]:
-                            # The player no more have a current team, we can continue.
-                            continue
+                if "currentTeam" not in player_info_json["people"][0]:
+                    # The player no more have a current team, we can continue.
+                    continue
 
-                        player_current_team = player_info_json["people"][0]["currentTeam"]["id"]
-                        players_id_to_current_team_dict[player_id] = player_current_team
-                    else:
-                        player_current_team = players_id_to_current_team_dict[player_id]
-                    
-                    if pool["context"]["players"][str(player_id)]["team"] != player_current_team: # player got traded.
-                        pool["context"]["players"][str(player_id)]["team"] = player_current_team
-                        print("{} got traded to the {}.".format(pool["context"]["players"][str(player_id)]["name"], player_current_team))
+                player_current_team = player_info_json["people"][0]["currentTeam"]["id"]
+                player_current_team_name = player_info_json["people"][0]["currentTeam"]["name"]
+                players_id_to_current_team_dict[player_id] = player_current_team
+            else:
+                player_current_team = players_id_to_current_team_dict[player_id]
+            
+            if pool["context"]["players"][str(player_id)]["team"] != player_current_team: # player got traded.
+                pool["context"]["players"][str(player_id)]["team"] = player_current_team
+                print("{} got traded to the {}.".format(pool["context"]["players"][str(player_id)]["name"], player_current_team_name))
 
-        db.pools.update_one({"name": pool["name"]}, {"$set": {f"context.pooler_roster": pool["context"]["pooler_roster"]}}, upsert=True)
+        db.pools.update_one({"name": pool["name"]}, {"$set": {f"context.players": pool["context"]["players"]}}, upsert=True)
                     
 
 if __name__ == "__main__":
